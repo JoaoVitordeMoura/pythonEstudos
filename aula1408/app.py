d6947b2ba2db4c4d50d39f750aaded5a339f8781
JoaoVitordeMoura <161031038+JoaoVitordeMoura@users.noreply.github.com>
2024-08-20 10:11:18 -0300

    Add files via upload

---
+++ aula1408/app.py
@@ -0,0 +1,44 @@
+#1
+print("Olá mundo")
+
+#2
+n1 = int(input("Digite o 1° número:"))
+n2 = int(input("Digite o 2° número:"))
+n3 = int(input("Digite o 3° número:"))
+resultado = n1 + n2 + n3
+print(resultado)
+
+#3
+
+fruta = input("Digite o nome de uma fruta: ")
+print("A fruta digitada foi:", fruta)
+
+#4
+
+km = float(input("Digite a distância da cidade: "))
+print (km * 1000)
+
+#5
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+print(media)
+
+#6
+
+lado = float(input("Digite um lado do quadrado: "))
+area = lado ** 2
+print(area)
+
+#7
+
+raio = float(input("Digite um valor para o raio: "))
+areaCirculo = (raio * 3.14) ** 2
+print(areaCirculo)
+
+#8
+
+fahrenheit = float(input("Digite uma temperatura: "))
+celsius = 5 * ((fahrenheit - 32) / 9) 
+print(celsius)
