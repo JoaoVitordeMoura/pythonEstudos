08ee8b7502e71b2bc8a8d193501a7d7d23c49bae
JoaoVitordeMoura <161031038+JoaoVitordeMoura@users.noreply.github.com>
2024-08-28 11:02:45 -0300

    Add files via upload

--- ex.py
+++ ex.py
@@ -69,10 +69,9 @@ else:
     print("Não é multiplo")'''
 
 #7
-
-str 
+ 
 vogais = "a" and "A" or "e" and "E" or "i" and "I" or "o" and "O" or "u" and "U"
-letra = str(input("Digite uma letra: "))
+letra = input("Digite uma letra: ")
 
 if letra == vogais:
     print("Vogal")
@@ -99,6 +98,70 @@ match dia:
     case 7:
         print("Sábado")
     case _:
-        print("Inválido")                 
-   
+        print("Inválido")      
+
+
+#9
+
+#10
+
+morango = float(input("Digite a quantidade de morangos em kg: "))
+maca = float(input("Digite a quantidade de maçã em kg:  "))
+precoMaca1 = 8.80
+precoMorango1 = 19.50
+precoMaca2 = 7.50
+precoMorango2 = 15.20
+
+if maca and morango <= 5:
+    compra = (morango * precoMorango1) + (maca * precoMaca1)
+    if compra > 25:
+        print(compra - (compra * 0.10))
+    else:
+        print(compra)
+elif maca and morango > 5:
+    compra = (morango * precoMorango2) + (maca * precoMaca2)
+    if compra > 25:
+        print(compra - (compra * 0.10))
+    else:
+        print(compra)
+elif morango <= 5:
+    compra = (morango * precoMorango1) + (maca * precoMaca2)
+    if compra > 25:
+        print(compra - (compra * 0.10))
+elif maca <= 5:
+    compra = (morango * precoMorango2) + (maca * precoMaca1)
+    if compra > 25:
+        print(compra - (compra * 0.10))
+
+
+#11
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+media = (n1 + n2) / 2
+
+if media >= 9:
+    conceito = "A"
+    if conceito == "A":
+        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
+elif media >= 7.5:
+    conceito = "B"
+    if conceito == "B":
+        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
+elif media >= 6:
+    conceito = "C"
+    if conceito == "C":
+        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
+elif media >= 4:
+    conceito = "D"
+    if conceito == "D":
+        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "REPROVADO")
+elif media < 4:
+    conceito = "E"
+    if conceito == "E":
+        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "REPROVADO")
+    
+
+
+
 
