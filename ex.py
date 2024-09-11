8ba21148c99f54e009f584cb580a81d42cc892ef
João Vitor <161031038+JoaoVitordeMoura@users.noreply.github.com>
2024-09-11 11:13:39 -0300

    Add files via upload

--- ex.py
+++ ex.py
@@ -1,320 +1,399 @@
-'''#1
+# #1
 
-numero = int(input("Digite um número inteiro: "))
+# numero = int(input("Digite um número inteiro: "))
 
-if numero % 2 == 0:
-    print("Par")
-else:
-    print("Ímpar")
+# if numero % 2 == 0:
+#     print("Par")
+# else:
+#     print("Ímpar")
 
-#2
+# #2
 
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
+# n1 = int(input("Digite um número: "))
+# n2 = int(input("Digite outro número: "))
 
-if n1 < n2:
-    print(n1)
-    print(n2)
-    print(n2)
-    print(n1)
-else:
-    print(n2)
-    print(n1)
-    print(n1)
-    print(n2)
+# if n1 < n2:
+#     print(n1)
+#     print(n2)
+#     print(n2)
+#     print(n1)
+# else:
+#     print(n2)
+#     print(n1)
+#     print(n1)
+#     print(n2)
 
 
-#3
+# #3
 
-x1 = int(input("Digite um número: "))
-x2 = int(input("Digite outro número: "))
+# x1 = int(input("Digite um número: "))
+# x2 = int(input("Digite outro número: "))
 
-if x1 > x2:
-    print(x1, "é maior e" , x2 , "é menor")
-else:
-    print(x2, "é maior e" , x1 , "é menor")
+# if x1 > x2:
+#     print(x1, "é maior e" , x2 , "é menor")
+# else:
+#     print(x2, "é maior e" , x1 , "é menor")
 
-#4
+# #4
 
-y1 = int(input("Digite o primeiro número: "))
-y2 = int(input("Digite o segundo número: "))
-y3 = int(input("Digite o terceiro número: "))
+# y1 = int(input("Digite o primeiro número: "))
+# y2 = int(input("Digite o segundo número: "))
+# y3 = int(input("Digite o terceiro número: "))
 
-if y1 < y2 < y3:
-    print(y1)
-elif y3 < y2 < y1:
-    print(y3)
-else:
-    print(y2)
+# if y1 < y2 < y3:
+#     print(y1)
+# elif y3 < y2 < y1:
+#     print(y3)
+# else:
+#     print(y2)
 
-#5
+# #5
 
-atacado = 10.90
-varejo = 12.90
-uni = int(input("Digite o tanto de unidades: "))
+# atacado = 10.90
+# varejo = 12.90
+# uni = int(input("Digite o tanto de unidades: "))
 
-if uni >= 20:
-    print(uni * atacado)
-else:
-    print(uni * varejo)
+# if uni >= 20:
+#     print(uni * atacado)
+# else:
+#     print(uni * varejo)
 
-#6
+# #6
 
-a = int(input("Digite o primeiro número inteiro: "))
-b = int(input("Digite o segundo número inteiro: "))
+# a = int(input("Digite o primeiro número inteiro: "))
+# b = int(input("Digite o segundo número inteiro: "))
 
-if a % b == 0:
-    print("É multiplo")
-else:
-    print("Não é multiplo")'''
+# if a % b == 0:
+#     print("É multiplo")
+# else:
+#     print("Não é multiplo")'''
 
-#7
+# #7
  
-'''vogais = "a" or "A" and "e" or "E" and "i" or "I" and "o" or "O" and "u" or "U"
-letra = input("Digite uma letra: ")
+# '''vogais = "a" or "A" and "e" or "E" and "i" or "I" and "o" or "O" and "u" or "U"
+# letra = input("Digite uma letra: ")
 
-if letra == vogais:
-    print("Vogal")
-else:
-    print("Consoante")
+# if letra == vogais:
+#     print("Vogal")
+# else:
+#     print("Consoante")
 
-#8
+# #8
 
-dia = int(input("Digite um dia de número: "))
+# dia = int(input("Digite um dia de número: "))
 
-match dia:
-    case 1:
-        print("Domingo")
-    case 2:
-        print("Segunda")
-    case 3:
-        print("Terça")
-    case 4:
-        print("Quarta")
-    case 5:
-        print("Quinta")
-    case 6:
-        print("Sexta")
-    case 7:
-        print("Sábado")
-    case _:
-        print("Inválido")      
-
-
-#9
-
-#10
-
-morango = float(input("Digite a quantidade de morangos em kg: "))
-maca = float(input("Digite a quantidade de maçã em kg:  "))
-precoMaca1 = 8.80
-precoMorango1 = 19.50
-precoMaca2 = 7.50
-precoMorango2 = 15.20
-
-if morango <= 5 and maca > 5:
-    compra = (morango * precoMorango1) + (maca * precoMaca2)
-    if compra > 25:
-        print(compra - (compra * 0.10))
-elif maca <= 5 and morango > 5:
-    compra = (morango * precoMorango2) + (maca * precoMaca1)
-    if compra > 25:
-        print(compra - (compra * 0.10))
-elif maca and morango <= 5:
-    compra = (morango * precoMorango1) + (maca * precoMaca1)
-    if compra > 25:
-        print(compra - (compra * 0.10))
-    else:
-        print(compra)
-elif maca and morango > 5:
-    compra = (morango * precoMorango2) + (maca * precoMaca2)
-    if compra > 25:
-        print(compra - (compra * 0.10))
-    else:
-        print(compra)
+# match dia:
+#     case 1:
+#         print("Domingo")
+#     case 2:
+#         print("Segunda")
+#     case 3:
+#         print("Terça")
+#     case 4:
+#         print("Quarta")
+#     case 5:
+#         print("Quinta")
+#     case 6:
+#         print("Sexta")
+#     case 7:
+#         print("Sábado")
+#     case _:
+#         print("Inválido")      
 
 
-#11
+# #9
 
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-media = (n1 + n2) / 2
+# #10
 
-if media >= 9:
-    conceito = "A"
-    if conceito == "A":
-        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
-elif media >= 7.5:
-    conceito = "B"
-    if conceito == "B":
-        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
-elif media >= 6:
-    conceito = "C"
-    if conceito == "C":
-        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
-elif media >= 4:
-    conceito = "D"
-    if conceito == "D":
-        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "REPROVADO")
-elif media < 4:
-    conceito = "E"
-    if conceito == "E":
-        print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "REPROVADO")
+# morango = float(input("Digite a quantidade de morangos em kg: "))
+# maca = float(input("Digite a quantidade de maçã em kg:  "))
+# precoMaca1 = 8.80
+# precoMorango1 = 19.50
+# precoMaca2 = 7.50
+# precoMorango2 = 15.20
+
+# if morango <= 5 and maca > 5:
+#     compra = (morango * precoMorango1) + (maca * precoMaca2)
+#     if compra > 25:
+#         print(compra - (compra * 0.10))
+# elif maca <= 5 and morango > 5:
+#     compra = (morango * precoMorango2) + (maca * precoMaca1)
+#     if compra > 25:
+#         print(compra - (compra * 0.10))
+# elif maca and morango <= 5:
+#     compra = (morango * precoMorango1) + (maca * precoMaca1)
+#     if compra > 25:
+#         print(compra - (compra * 0.10))
+#     else:
+#         print(compra)
+# elif maca and morango > 5:
+#     compra = (morango * precoMorango2) + (maca * precoMaca2)
+#     if compra > 25:
+#         print(compra - (compra * 0.10))
+#     else:
+#         print(compra)
+
+
+# #11
+
+# n1 = float(input("Digite a primeira nota: "))
+# n2 = float(input("Digite a segunda nota: "))
+# media = (n1 + n2) / 2
+
+# if media >= 9:
+#     conceito = "A"
+#     if conceito == "A":
+#         print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
+# elif media >= 7.5:
+#     conceito = "B"
+#     if conceito == "B":
+#         print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
+# elif media >= 6:
+#     conceito = "C"
+#     if conceito == "C":
+#         print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "APROVADO")
+# elif media >= 4:
+#     conceito = "D"
+#     if conceito == "D":
+#         print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "REPROVADO")
+# elif media < 4:
+#     conceito = "E"
+#     if conceito == "E":
+#         print("Nota 1:" , n1 , "Nota 2:" , n2 , "Média:" , media , "Conceito:" , conceito , "Resultado:" , "REPROVADO")
     
 
-#12
+# #12
 
-numero = int(input("Digite um número inteiro e de três dígitos: "))
-n3 = numero % 10
-numero = numero // 10
-n2 = numero % 10
-numero = numero // 10
-n1 = numero
+# numero = int(input("Digite um número inteiro e de três dígitos: "))
+# n3 = numero % 10
+# numero = numero // 10
+# n2 = numero % 10
+# numero = numero // 10
+# n1 = numero
 
-if n1 < n2 < n3:
-    print("Crescente!")
-else:
-    print("Decrescente!")
+# if n1 < n2 < n3:
+#     print("Crescente!")
+# else:
+#     print("Decrescente!")
 
-#laço de repetição
+# #laço de repetição
 
-for i in range(0 , 10):
-    print(i)
+# for i in range(0 , 10):
+#     print(i)
 
-s = "python"
+# s = "python"
 
-for s in str(s):
-    print(s)
+# for s in str(s):
+#     print(s)
 
-s = "python"
+# s = "python"
 
-for i, c in enumerate(s, 1):
-    print(i, c)
+# for i, c in enumerate(s, 1):
+#     print(i, c)
 
-x = int(input("Digite um número positivo: "))
+# x = int(input("Digite um número positivo: "))
 
-while x < 0:
-    x = int(input("Invalido, digite novamente: "))
+# while x < 0:
+#     x = int(input("Invalido, digite novamente: "))
 
-contador = 0
+# contador = 0
 
-while contador < 100:
-    print("Curso de python")
-    contador +=1
-print(contador)
+# while contador < 100:
+#     print("Curso de python")
+#     contador +=1
+# print(contador)
 
-i = 0
-contador = 0
+# i = 0
+# contador = 0
 
-while i < 10:
-    x = int(input("Digite um número: "))
-    if x >= 0:
-        contador += 1
-    i += 1    
-print(contador)
+# while i < 10:
+#     x = int(input("Digite um número: "))
+#     if x >= 0:
+#         contador += 1
+#     i += 1    
+# print(contador)
 
-s = "joao"
+# s = "joao"
 
-for c in s:
-    if c == 'o':
-        continue
-    print(c)
+# for c in s:
+#     if c == 'o':
+#         continue
+#     print(c)
 
-for i in range(10):
-    print(i)
-else:
-    print("Fim do laço")
+# for i in range(10):
+#     print(i)
+# else:
+#     print("Fim do laço")
 
-#ex laço de repetição
+# #ex laço de repetição
 
-#a
-for i in range(1 , 101):
-    print(i)
+# #a
+# for i in range(1 , 101):
+#     print(i)
 
-#b
-for i in range(20 , 41):
-    print(i)
+# #b
+# for i in range(20 , 41):
+#     print(i)
 
-#c
+# #c
 
-soma = 0
-numero = 20
+# soma = 0
+# numero = 20
 
-while numero <= 40:
-    soma += numero
-    numero += 1
-    print(soma)
+# while numero <= 40:
+#     soma += numero
+#     numero += 1
+#     print(soma)
 
-#D
+# #D
 
-soma1 = 0
-numero1 = -20
+# soma1 = 0
+# numero1 = -20
 
-while numero1 <= 10:
-    soma1 += numero1
-    numero1 += 1
-    print(soma1)
+# while numero1 <= 10:
+#     soma1 += numero1
+#     numero1 += 1
+#     print(soma1)
 
-#2A
+# #2A
 
-for i in range(1 , 51):
-    if i % 2 == 0:
+# for i in range(1 , 51):
+#     if i % 2 == 0:
     
-        print(i)
-    else:
-        continue
+#         print(i)
+#     else:
+#         continue
 
-#2B
+# #2B
 
-for i in range(50 , 100):
-    if i % 2 == 0:
-        continue
-    print(i)
+# for i in range(50 , 100):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
 
-#2C
+# #2C
 
-for i in range(1 , 100):
-    if i % 3 == 0:
-        print(i)
-    else:
-        continue
+# for i in range(1 , 100):
+#     if i % 3 == 0:
+#         print(i)
+#     else:
+#         continue
 
-#2D
+# #2D
 
-for i in range(50 , 200):
-    if i % 7 == 0:
-        print(i)
-    else:
-        continue
+# for i in range(50 , 200):
+#     if i % 7 == 0:
+#         print(i)
+#     else:
+#         continue
 
 
-#3A
+# #3A
 
-soma = 0
-numero = 50
+# soma = 0
+# numero = 50
 
-while numero <= 150:
-    soma += numero
-    numero += 2
-    print(soma)
+# while numero <= 150:
+#     soma += numero
+#     numero += 2
+#     print(soma)
 
-#3B
+# #3B
 
-for i in range(1 , 101):
-    if i % 3 == 0:
-        print(i)
-    elif i % 5 == 0:
-        print(i)
-    else:
-        continue'''
+# for i in range(1 , 101):
+#     if i % 3 == 0:
+#         print(i)
+#     elif i % 5 == 0:
+#         print(i)
+#     else:
+#         continue
 
-#3C
+# #3C
 
-x = 1
+# x = 1
 
-for i , c in enumerate(1 , 500):
-    if i % 5 == 0:
-        print(i)
+# for i , c in enumerate(1 , 500):
+#     if i % 5 == 0:
+#         print(i)
+
+# 4A
+
+# for i in range(0 , 11):
+#     i = i * 2
+#     print(i)
+
+# 4B
+
+# for i in range(0 , 11):
+#     i = i * 5
+#     print(i)
+
+# 4C
+
+# for i in range(0 , 11):
+#     i = i * 7
+#     print(i)
+
+# 5
+
+# s = "Paralelepipedo"
+
+# for i, c in enumerate(s, 1):
+#      print(i, c)
+
+# 6
+
+# a = "Curso"
+# b = "Python"
+
+# for i in range(1 , 11):
+#     print(a)
+#     print(b)
+
+# 7
+
+# nota = float(input("Digite uma nota: "))
+
+# while nota < 0 or nota > 10:
+#     nota = float(input("Digite novamente: "))
+
+# print(nota)
+
+# 8
+
+idadeNova = 200
+idadeVelha = 0
+idade = int(input("Idade (idade negativa termina a leitura)"))
+while idade > 0:
+    idade = int(input("Idade (idade negativa termina a leitura)"))
+    if idade < idadeNova:
+        idadeNova = idade
+    if idade > idadeVelha:
+        idadeVelha = idade
+print("Nova: " , idadeNova)
+print("Velha: ", idadeVelha)
+
+# 9
+
+# c = "python"
+
+
+# 10
+
+# n = int(input("Digite um número inteiro: "))
+
+# while n < 0:
+#     n = int(input("Número inválido, digite novamente: "))
+
+
+
+
+
+
+
+
+
+
 
 
 
